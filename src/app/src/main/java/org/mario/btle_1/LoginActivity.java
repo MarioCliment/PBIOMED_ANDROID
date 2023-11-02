b253fdd06bd08e6884de082e59a015933d222a57
Ignacio <iyusazn@epsg.upv.es>
2023-11-02 12:34:56 +0100

    LOGIN
    LOGIN FUNCIONA juejuejuejue

--- src/app/src/main/java/org/mario/btle_1/LoginActivity.java
+++ src/app/src/main/java/org/mario/btle_1/LoginActivity.java
@@ -2,20 +2,42 @@ package org.mario.btle_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private String server = "http://192.168.1.133:80/pruebaRESTful/rest/";
+
+    private String server_especifico = server + "hacerLogin.php";
+
+    private boolean resultado = false;
+    EditText password;
+    EditText user;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        getSupportActionBar().hide();
+        user = findViewById(R.id.usuario);
+        password = findViewById(R.id.contrasenya);
+        loginBtn = findViewById(R.id.botonLogin);
+
+        loginBtn.setOnClickListener(v->hacerLogin());
 
         TextView textView = findViewById(R.id.irRegistrar);
 
@@ -34,5 +56,53 @@ public class LoginActivity extends AppCompatActivity {
 
         // Establecer el texto formateado en el TextView
         textView.setText(spannableString);
+    }
+
+    void hacerLogin(){
+
+        String userS = this.user.getText().toString().trim();
+        String passwordS = this.password.getText().toString();
+
+        //String data = "?user="+userS+"&password="+passwordS;
+
+        JSONObject objeto = new JSONObject();
+        try {
+            objeto.put("user", userS);
+            objeto.put("password", passwordS);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        String data = objeto.toString();
+
+        PeticionarioREST elPeticionario = new PeticionarioREST();
+        elPeticionario.hacerPeticionREST("POST", server_especifico, data,
+                new PeticionarioREST.RespuestaREST() {
+                    @Override
+                    public void callback(int codigo, String cuerpo) {
+                        String jsonString = cuerpo;
+                        try {
+                            // Convierte la cadena JSON en un objeto JSON
+                            JSONObject jsonObject = new JSONObject(jsonString);
+
+                            // Extrae datos específicos del objeto JSON
+                            resultado = jsonObject.getBoolean("resultado");
+                            Log.d("resultado",""+resultado);
+                            if (resultado == true){
+                                irMainActivity();
+                            }
+
+                            // Haz algo con los datos extraídos
+                            // Por ejemplo, muestra los datos en una vista
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            // Maneja errores de análisis JSON aquí
+                        }
+                    }
+                });
+    }
+
+    void irMainActivity(){
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
