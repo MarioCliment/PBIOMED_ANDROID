fecc3fa20840eedc36b49fac22aacd445f1fe6a2
Dr. Kraken <lavafrita@gmail.com>
2023-11-03 12:36:52 +0100

    hecho majete

--- src/app/src/main/java/org/mario/btle_1/MainActivity.java
+++ src/app/src/main/java/org/mario/btle_1/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.ACCESS_FINE_LOCATION,
     };
 
-    private String server = "http://192.168.1.133:80/ozonewarden/rest/";
+    private String server = "http://192.168.88.7:80/ozonewarden/rest/";
 
     private TextView elTexto;
 
