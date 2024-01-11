d67c4d6e214af0596adc06eb4261918317c7f5fe
Dr. Kraken <lavafrita@gmail.com>
2024-01-11 23:04:10 +0100

    Mario toma mi energia !!!
    activity de has olvidado tu contraseña, ¡terminada! Solo queda que MARIO CLIMENT JIMENEZ:
    1ºAgregue la comprobacion de que el email se ha enviado correctamente
    2ºEnvie el email jijijiji!

--- src/app/src/main/java/org/mario/btle_1/EditPassActivity.java
+++ src/app/src/main/java/org/mario/btle_1/EditPassActivity.java
@@ -1,43 +1,96 @@
 package org.mario.btle_1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 public class EditPassActivity extends AppCompatActivity {
     //private String server = "http://192.168.1.140:80/PBIOMED_SERVIDOR/src/rest"; //CASA MAYRO
 
-    //private String server = "http://192.168.1.148:80/PBIOMED_SERVIDOR/src/rest"; //CASA GRASA
+
+    //private String server = "http://192.168.1.148:80/PBIOMED_SERVIDOR/src/rest"; //CASA GRASA // Ermmm porque esta mi casa grabada??? Lo reportare como una brecha de privacidad, je!
     private String server = "http://192.168.10.7:80/PBIOMED_SERVIDOR/src/rest"; //MOVIL MAYRO
 
-    EditText newpasstext;
-    EditText newpasstext2;
-    TextView avisoPass;
+    EditText emailTextView;
+    boolean logueado;
     private boolean resultado = false;
     private String server_registro = server + "/user/password";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
-        newpasstext = findViewById(R.id.NewPass);
-        newpasstext2 = findViewById(R.id.NewPass2);
-        avisoPass = findViewById(R.id.avisoPass);
-
+        emailTextView = findViewById(R.id.cambiarPassEmailTextView);
     }
-    public void cancelarPass(View view){
-        Intent intent = new Intent(this, EditUserActivity.class);
+
+    public void botonCancelar(View view){
+        irALogin();
+    }
+
+    private void irALogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
-    public void guardarPass(View view){
+
+
+    // HOLA MARIO!!! SALUDA!!! SALUDA DE VUELTA ESCRIBIENDO UN COMENTARIO AQUI ABAJO!!!
+    //
+    public void enviarEmailButton(View view) {
+        AlertDialog.Builder alertaEstadoEmail = new AlertDialog.Builder(EditPassActivity.this);
+        String email = this.emailTextView.getText().toString().trim();
+        if (validateData(email)) {
+            // funcionar
+            if (true) { // reemplazar este "true" por "correoEnviadoConExito", que comprueba si hemos enviado un correo o si hemos fallado
+                // mostrar "El correo se ha enviado con exito"
+                alertaEstadoEmail.setMessage("El correo se ha enviado con éxito, compruebe su e-mail ")// yuh uh!
+                        .setCancelable(false)
+                        .setNeutralButton("Entendido", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                                irALogin();
+                            }
+                        });
+                alertaEstadoEmail.show();
+            } else {
+                // mostrar "Ha ocurrido un error"
+                alertaEstadoEmail.setMessage("No se ha podido enviar el e-mail, inténtelo más tarde") // nuh uh!
+                        .setCancelable(false)
+                        .setNeutralButton("Entendido", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                alertaEstadoEmail.show();
+            }
+
+        } else {
+            // mostrar en pantalla "El email esta escrito incorrectamente!"
+            alertaEstadoEmail.setMessage("El email esta escrito incorrectamente ") // Meeeeec!
+                    .setCancelable(false)
+                    .setNeutralButton("Entendido", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                        }
+                    });
+            alertaEstadoEmail.show();
+        }
+    }
+
+
+    /*public void guardarPass(View view){
         if(newpasstext.getText() == newpasstext2.getText()){
             if(true){ // aqui antes ponia ''oldpasstext.getText().toString() == oldpassword'', pero ya no usamos "oldpassword"
                 JSONObject objeto = new JSONObject();
@@ -89,5 +142,13 @@ public class EditPassActivity extends AppCompatActivity {
             avisoPass.setVisibility(View.VISIBLE);
             avisoPass.setTextColor(Color.parseColor("#C62222"));
         }
+    }*/
+
+    public boolean validateData(String email) {
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            this.emailTextView.setError("El email no es valido");
+            return false;
+        }
+        return true;
     }
 }
